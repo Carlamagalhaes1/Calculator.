a93a0181f7f318e93b983465c3cb9027d99bb15b
Carla Magalhães <carlamagalhaes60@gmail.com>
2024-11-18 20:12:38 -0300

    mudando app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Calculator from './components/Calculator';
+import { CssBaseline, Container } from '@mui/material';
+import { GlobalStyles } from './styles/globalStyles';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <>
+    <CssBaseline />
+    <GlobalStyles />
+    <Container maxWidth="sm">
+      <Calculator />
+    </Container>
+  </>
+);
 
 export default App;
